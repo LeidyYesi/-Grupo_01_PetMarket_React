f961fb72298f1e1aed2725183f02b6bca2a67ddb
MartinFaraguna <faraguna.martin@gmail.com>
2023-04-11 23:41:59 -0300

    Quick Fix

--- src/component/ListadoProductos.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function ListadoProductos() {
-  return (
-    <div>ListadoProductos</div>
-  )
-}
-
-export default ListadoProductos
--- src/component/UltimoProducto.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function UltimoProducto() {
-  return (
-    <div>UltimoProducto</div>
-  )
-}
-
-export default UltimoProducto
