7cf6b1d05ca859719106d990c2054f9b92b21611
MartinFaraguna <faraguna.martin@gmail.com>
2023-04-13 16:12:53 -0300

    total usuarios y ultimo usuario registrado

--- src/components/ContentRowMovies.js
+++ src/components/ContentRowMovies.js
@@ -1,50 +1,64 @@
-import React from 'react';
-import SmallCard from './SmallCard';
-
+import React, { useEffect, useState } from "react";
+import SmallCard from "./SmallCard";
 /*  Cada set de datos es un objeto literal */
 
 /* <!-- Movies in DB --> */
 
 let moviesInDB = {
-    title: 'Movies in Data Base',
-    color: 'primary', 
-    cuantity: 21,
-    icon: 'fa-clipboard-list'
-}
+  title: "Movies in Data Base",
+  color: "primary",
+  cuantity: 21,
+  icon: "fa-clipboard-list",
+};
 
 /* <!-- Total awards --> */
 
-let totalAwards = {
-    title:' Total awards', 
-    color:'success', 
-    cuantity: '79',
-    icon:'fa-award'
-}
+// let totalAwards = {
+//   title: " Total awards",
+//   color: "success",
+//   cuantity: "79",
+//   icon: "fa-award",
+// };
 
 /* <!-- Actors quantity --> */
 
 let actorsQuantity = {
-    title:'Actors quantity' ,
-    color:'warning',
-    cuantity:'49',
-    icon:'fa-user-check'
-}
+  title: "Actors quantity",
+  color: "warning",
+  cuantity: "49",
+  icon: "fa-user-check",
+};
 
-let cartProps = [moviesInDB, totalAwards, actorsQuantity];
+function ContentRowMovies() {
+  const [users, setUsers] = useState([]);
 
-function ContentRowMovies(){
-    return (
-    
-        <div className="row">
-            
-            {cartProps.map( (movie, i) => {
+  useEffect(() => {
+    fetch("http://localhost:3030/api/users")
+      .then((response) => response.json())
+      .then((data) => {
+        setUsers(data.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
-                return <SmallCard {...movie} key={i}/>
-            
-            })}
+  let totalUsers = {
+    title: " Total users",
+    color: "success",
+    cuantity: users.length,
+    icon: "fa-user",
+  };
 
-        </div>
-    )
+  let cartProps = [moviesInDB, totalUsers, actorsQuantity];
+
+  return (
+    <div className="row">
+      {cartProps.map((movie, i) => {
+        return <SmallCard {...movie} key={i} />;
+      })}
+    </div>
+  );
 }
 
 export default ContentRowMovies;
--- src/components/LastMovieInDb.js
+++ src/components/LastMovieInDb.js
@@ -1,23 +1,41 @@
-import React from 'react';
-import imagenFondo from '../assets/images/mandalorian.jpg';
+import React, { useEffect, useState } from "react";
 
-function LastMovieInDb(){
-    return(
-        <div className="col-lg-6 mb-4">
-            <div className="card shadow mb-4">
-                <div className="card-header py-3">
-                    <h5 className="m-0 font-weight-bold text-gray-800">Last movie in Data Base</h5>
-                </div>
-                <div className="card-body">
-                    <div className="text-center">
-                        <img className="img-fluid px-3 px-sm-4 mt-3 mb-4" style={{width: 40 +'rem'}} src={imagenFondo} alt=" Star Wars - Mandalorian "/>
-                    </div>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, consequatur explicabo officia inventore libero veritatis iure voluptate reiciendis a magnam, vitae, aperiam voluptatum non corporis quae dolorem culpa citationem ratione aperiam voluptatum non corporis ratione aperiam voluptatum quae dolorem culpa ratione aperiam voluptatum?</p>
-                    <a className="btn btn-danger" target="_blank" rel="nofollow" href="/">View movie detail</a>
-                </div>
-            </div>
+function LastMovieInDb() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3030/api/users")
+      .then((response) => response.json())
+      .then((data) => {
+        setUsers(data.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  const ultimoUsuario = users[users.length - 1];
+
+  if (ultimoUsuario === undefined) {
+    return <h1>Cargando pagina...</h1>;
+  }
+
+  return (
+    <div className="col-lg-6 mb-4">
+      <div className="card shadow mb-4">
+        <div className="card-header py-3">
+          <h5 className="m-0 font-weight-bold text-gray-800 text-center">
+            Ultimo usuario registrado
+          </h5>
         </div>
-    )
+        <div className="card-body">
+          <div className="text-center">
+            <h3>{ultimoUsuario.name} {ultimoUsuario.lastname}</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default LastMovieInDb;
