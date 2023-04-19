dc099ff324242db9b6bfce467c59a5f91f017956
MartinFaraguna <faraguna.martin@gmail.com>
2023-04-18 23:00:19 -0300

    category y cantidad

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,67 +1,58 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Category from "./Category";
 
 function Categories() {
-  let CategotyData = [
-    {
-      category: "Accesorios",
-      products: "6",
-    },
-    {
-      category: "Juguetes",
-      products: "1",
-    },
-    {
-      category: "Alimentos",
-      products: "1",
-    },
-    {
-      category: "Aseo",
-      products: "2",
-    },
-  ];
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3030/api/products")
+      .then((response) => response.json())
+      .then((data) => {
+        setProducts(data.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  const countCategories = (products) => {
+    const categoriesCount = {};
+
+    products.forEach((product) => {
+      if (!categoriesCount.hasOwnProperty(product.category)) {
+        categoriesCount[product.category] = 1;
+      } else {
+        categoriesCount[product.category]++;
+      }
+    });
+
+    return categoriesCount;
+  };
+
+  const categoriesCount = countCategories(products);
 
   return (
     <div className="col-lg-6 mb-4">
       <div className="card shadow mb-4">
         <div className="card-header py-3">
-          <h5 className="m-0 font-weight-bold text-gray-800 text-primary-emphasis">
+          <h5 className="m-0 font-weight-bold text-dark">
             Categorias de Productos
           </h5>
         </div>
         <div className="card-body">
           <div className="row">
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-sarasa text-white shadow">
-                <div className="card-body">
-                <Category category="Juguetes" products="1"/>
+            {Object.entries(categoriesCount).map(
+              ([category, productsCount], index) => (
+                <div className="col-lg-6 mb-4" key={index}>
+                  <div className="card bg-sarasa text-white shadow">
+                    <div className="card-body">
+                      {console.log(category + " / " + productsCount)}
+                      <Category category={category} products={productsCount} />
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </div>
-          
-          <div className="col-lg-6 mb-4">
-            <div className="card bg-sarasa text-white shadow">
-              <div className="card-body">
-              <Category category="Accesorios" products="6"/>
-              </div>
-            </div>
-          </div>
-
-          <div className="col-lg-6 mb-4">
-            <div className="card bg-sarasa text-white shadow">
-              <div className="card-body">
-              <Category category="Aseo" products="2"/>
-                </div>
-            </div>
-          </div>
-
-          <div className="col-lg-6 mb-4">
-            <div className="card bg-sarasa text-white shadow">
-              <div className="card-body">
-              <Category category="Alimentos" products="1"/>
-                </div>
-            </div>
-          </div>
+              )
+            )}
           </div>
         </div>
       </div>
--- src/components/LastUsers.jsx
+++ src/components/LastUsers.jsx
@@ -15,7 +15,7 @@ function LastUsers() {
   }, []);
 
   const ultimoUsuario = users[users.length - 1];
-  const localhost = "http://localhost:3030"
+  const localhost = "http://localhost:3030";
 
   if (ultimoUsuario === undefined) {
     return <h1>Cargando pagina...</h1>;
@@ -25,19 +25,19 @@ function LastUsers() {
     <div className="col-lg-6 mb-4">
       <div className="card shadow mb-4">
         <div className="card-header py-3">
-          <h5 className="m-0 font-weight-bold text-gray-800 text-center">
+          <h5 className="m-0 font-weight-bold text-center text-dark">
             Ultimo usuario registrado
           </h5>
         </div>
         <div className="card-body">
-          <div className="text-center">
+          <div className="text-center font-weight-bold text-dark">
             <h3>
               {ultimoUsuario.name} {ultimoUsuario.lastname}
             </h3>
             <img
               className="img-fluid px-3 px-sm-4 mt-3 mb-4"
               style={{ width: 40 + "rem" }}
-              src= {localhost + ultimoUsuario.img}
+              src={localhost + ultimoUsuario.img}
               alt=" Foto de perfil "
             />
           </div>
