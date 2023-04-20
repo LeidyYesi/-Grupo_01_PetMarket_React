70619a06038e991d3472d49b3f37e07f8cce8f9c
leidyyesi <cantamosaleluya@gmail.com>
2023-04-20 09:18:24 -0300

    Actualización final del footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,46 +1,60 @@
-import React from 'react';
-import {Link, Route, Switch} from 'react-router-dom';
+import React from "react";
+import { Link, Route, Switch } from "react-router-dom";
 
-function Footer(){
-    return (
-        <React.Fragment>
-			<footer className="sticky-footer bg-white">
-			<section class="border-top">
-        <div class="container text-center text-md-start mt-5">
-            <div class="row mt-3">
-                <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-					<Link className="nav-link" to="/ContentRowMovies">
-                        <i className="fas fa-fw fa-table text-warning"></i>
-                        <span className="text-dark">Productos</span>
-                    </Link> 
-            
-                </div>
-                <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-				<Link className="nav-link" to="/GenresInDb">
-                        <i className="fas fa-fw fa-folder text-warning"></i>
-                        <span className="text-dark">Categorias</span>
-                    </Link>
-                </div>
+function Footer() {
+  return (
+    <React.Fragment>
+      <footer className="sticky-footer bg-white">
+        <section className="border-top">
+          <div className="container text-center text-md-start mt-5">
+            <div className="row mt-3">
+              <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                <i className="fab fa-linkedin text-warning"></i>
+                <a
+                  href="https://www.linkedin.com/in/yesica-farias-b45760115/"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                >
+                  <span className="text-dark">Yesica Farias</span>
+                </a>
+              </div>
 
-                <div class="col-md-4 col-lg-2 col-xl-2 mx-auto mb-md-0 mb-4">
-				<Link className="nav-link" to="/LastMovieInDb">
-                        <i className="fas fa-fw fa-chart-area text-warning"></i>
-                        <span className="text-dark">Usuarios</span>
-                </Link>
-                
-                </div>
-                
+              <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                <i className="fab fa-linkedin text-warning"></i>
+                <a
+                  href="https://www.linkedin.com/in/carlos-andres-nore%C3%B1a-cruz-3a689983/"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                >
+                  <span className="text-dark">Carlos Noreña</span>
+                </a>
+              </div>
+
+              <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                <i className="fab fa-linkedin text-warning"></i>
+                <a
+                  href="https://www.linkedin.com/in/mart%C3%ADn-faraguna-6ab136196/"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                >
+                  <span className="text-dark">Martin Faraguna</span>
+                </a>
+              </div>
+
+              <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                <i className="fab fa-linkedin text-warning"></i>
+                <span className="text-dark">German</span>
+              </div>
             </div>
+          </div>
+        </section>
+        <div className="container my-auto">
+          <div className="copyright text-center my-auto">
+            <span>Copyright &copy; 2023DHGroup1.com </span>
+          </div>
         </div>
-    </section>
-				<div className="container my-auto">
-					<div className="copyright text-center my-auto">
-						<span>Copyright &copy; 2023DHGroup1.com </span>
-					</div>
-				</div>
-			</footer>
-
-        </React.Fragment>
-    )
+      </footer>
+    </React.Fragment>
+  );
 }
 export default Footer;
